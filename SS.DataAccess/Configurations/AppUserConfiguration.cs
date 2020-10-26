49cf307bdcda96413a18926a8b23f6cc2ffc6e92
Hoang Truong <hqtruong27@gmail.com>
2020-10-26 22:04:13 +0700

    new table identityServer

---
+++ SS.DataAccess/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SS.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SS.DataAccess.Configurations
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.ToTable("AppUsers");
+            builder.Property(x => x.FullName).HasMaxLength(250).IsRequired();
+        }
+    }
+}
