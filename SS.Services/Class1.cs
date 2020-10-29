7c11eedbf3ad787e91b7b0afe4ed22c47cd536cf
Hoang Truong <hqtruong27@gmail.com>
2020-10-29 11:40:47 +0700

    manager product, create, update, pagging and select product by categoryId

--- SS.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SS.Services
-{
-    public class Class1
-    {
-    }
-}
---
+++ SS.Utilities/Exceptions/SufeeException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SS.Utilities.Exceptions
+{
+    public class SufeeException : Exception
+    {
+        public SufeeException()
+        {
+        }
+
+        public SufeeException(string message)
+            : base(message)
+        {
+        }
+
+        public SufeeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
