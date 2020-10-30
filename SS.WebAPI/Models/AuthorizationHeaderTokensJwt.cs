8f0e1a50df357d627aadcb80ae010c365f57e837
Hoang Truong <hqtruong27@gmail.com>
2020-10-30 21:46:52 +0700

    nochange

--- SS.WebAPI/Models/AuthorizationHeaderTokensJwt.cs
+++ SS.WebAPI/Models/AuthorizationHeaderTokensJwt.cs
@@ -3,10 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SS.WebAPI.Models
 {
