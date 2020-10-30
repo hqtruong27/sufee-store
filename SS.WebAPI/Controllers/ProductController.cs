08c5160972b8e9093a10e236b8c1985a56ee1931
Hoang Truong <hqtruong27@gmail.com>
2020-10-30 21:46:21 +0700

    nochange

--- SS.WebAPI/Controllers/ProductController.cs
+++ SS.WebAPI/Controllers/ProductController.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-<<<<<<< HEAD
-=======
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
->>>>>>> origin/develop
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SS.Services.Interface.IProductServices;
 
 namespace SS.WebAPI.Controllers
 {
-<<<<<<< HEAD
-    [Route("api/[controller]")]
-    [ApiController]
-=======
     [ApiController]
     [Route("api/[controller]")]
->>>>>>> origin/develop
     public class ProductController : ControllerBase
     {
         private readonly IClientProduct _product;
@@ -27,10 +15,7 @@ namespace SS.WebAPI.Controllers
             _product = product;
         }
         [HttpGet]
-<<<<<<< HEAD
-=======
         [Authorize]
->>>>>>> origin/develop
         public async Task<IActionResult> GetProduct()
             => Ok(await _product.GetProduct());
     }
--- SS.WebAPI/Startup.cs
+++ SS.WebAPI/Startup.cs
@@ -1,29 +1,10 @@
-<<<<<<< HEAD
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-=======
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
->>>>>>> origin/develop
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-<<<<<<< HEAD
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using SS.DataAccess.EF;
-using SS.Services.Interface.IProductServices;
-using SS.Services.Services.ProductServices;
-using SS.Utilities.Constants;
-=======
 using SS.DataAccess.EF;
 using SS.DataAccess.Entities;
 using SS.Services.Interface.IProductServices;
@@ -32,7 +13,6 @@ using SS.Services.Services.ProductServices;
 using SS.Services.Services.UserServices;
 using SS.Utilities.Constants;
 using SS.WebAPI.Models;
->>>>>>> origin/develop
 
 namespace SS.WebAPI
 {
@@ -42,10 +22,6 @@ namespace SS.WebAPI
         {
             Configuration = configuration;
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/develop
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -53,19 +29,6 @@ namespace SS.WebAPI
         {
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString(SystemConstants.NameConnectionString)));
-<<<<<<< HEAD
-
-            //DI
-            services.AddTransient<IClientProduct, ClientProductServices>();
-
-            services.AddControllers();
-
-            //Swagger
-            services.AddSwaggerGen(x =>
-            {
-                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Api Sufee", Version = "v1" });
-            });
-=======
             services.AddIdentity<AppUser, IdentityRole>().
                 AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
@@ -80,7 +43,6 @@ namespace SS.WebAPI
 
             //Swagger + Jwt Bearer
             services.ConfigureAuthentication(Configuration);
->>>>>>> origin/develop
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -94,11 +56,6 @@ namespace SS.WebAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
-<<<<<<< HEAD
-
-            //swagger
-            app.UseSwagger();
-=======
             //Authen before Authorize
             app.UseAuthentication();
 
@@ -107,16 +64,11 @@ namespace SS.WebAPI
             //swagger
             app.UseSwagger();
 
->>>>>>> origin/develop
             app.UseSwaggerUI(x =>
             {
                 x.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Sufee_Store_v1");
             });
 
-<<<<<<< HEAD
-            app.UseAuthorization();
-=======
->>>>>>> origin/develop
 
             app.UseEndpoints(endpoints =>
             {
